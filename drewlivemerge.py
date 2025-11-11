febc0f5f6c12ae594f8119b43849e4b004bc2ed1
Bainchiclo <baniknikinchiclo@gmail.com>
2025-11-11 17:00:08 +0800

    Update drewlivemerge.py

--- drewlivemerge.py
+++ drewlivemerge.py
@@ -6,6 +6,7 @@ from datetime import datetime
 playlist_urls = [
     "https://raw.githubusercontent.com/Bainchiclo/nickiptv/refs/heads/main/PH.m3u",
     "https://raw.githubusercontent.com/Bainchiclo/nickiptv/refs/heads/main/NBA%20HOMECOURT.m3u",
+    "https://raw.githubusercontent.com/Bainchiclo/nickiptv/refs/heads/main/Pixelsports.m3u8",
     "https://raw.githubusercontent.com/Bainchiclo/nickiptv/refs/heads/main/PPV.m3u8"
 ]
 
