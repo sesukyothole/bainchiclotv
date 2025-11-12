1e1c5624de610816cb7d343d4dcf91dede5ad3ba
Bainchiclo <baniknikinchiclo@gmail.com>
2025-11-12 14:50:29 +0800

    Add new playlist URL to drewlivemerge.py

--- drewlivemerge.py
+++ drewlivemerge.py
@@ -6,6 +6,7 @@ from datetime import datetime
 playlist_urls = [
     "https://raw.githubusercontent.com/Bainchiclo/nickiptv/refs/heads/main/PH.m3u",
     "https://raw.githubusercontent.com/Bainchiclo/nickiptv/refs/heads/main/NBA%20HOMECOURT.m3u",
+    "https://mc-o0of718lw0.bunny.run/playlist.m3u8",
     "https://raw.githubusercontent.com/Bainchiclo/nickiptv/refs/heads/main/Roxiestreams.m3u8",
     "https://raw.githubusercontent.com/Bainchiclo/nickiptv/refs/heads/main/Pixelsports.m3u8"
 ] 
